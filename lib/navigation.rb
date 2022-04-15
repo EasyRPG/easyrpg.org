585f6ede0e72ee09407920d2162ad5ea5e346c24
Carsten Teibes <dev@f4ke.de>
2022-04-15 15:28:08 +0200

    Move section indices into section More intuitive menu

--- lib/navigation.rb
+++ lib/navigation.rb
@@ -4,20 +4,11 @@ include Nanoc::Helpers::Rendering
 # generate links to specific items
 include Nanoc::Helpers::LinkTo
 
-def link_to_upper_page(text, attributes = {})
+def link_to_upper_page(attributes = {})
   p = @item_rep.path
 
-  # link from each subsection to section
-  if p =~ /(\/[^\/]*\/)[^\/]*\//
-    target = $1
-
-    if target == "/contribute/"
-      upper = "Introduction"
-    else
-      upper = "Overview"
-    end
   # link to homepage
-  elsif p =~ /\/about/
+  if p =~ /\/about/
     target = "/"
     upper = "Homepage"
   # link from each section to about page
@@ -29,40 +20,47 @@ def link_to_upper_page(text, attributes = {})
   attributes[:class] = "upper"
   attributes[:title] = "Back to #{upper} page"
 
-  link_to(text + " " + upper + " " + text, target, attributes)
+  link_to("&orarr; " + upper, target, attributes)
 end
 
 def navigation_for(item)
   # global: e.g. player
   section = item.identifier.components[0]
 
-  # strip ext if section front page
-  if section == item.identifier.to_s[1..] then
-    section = item.identifier.without_ext[1..]
-  end
-
   # make path
   if section == "about" then
     # special case: is the global section
     section = "/"
+    search = "*/index.*"
     part = 0
   else
     section = "/" + section + "/"
+    search = "*"
     part = 1
   end
 
   # get all sections members
   h = Hash.new
-  @items.find_all(section + '*').each do |it|
+  @items.find_all(section + search).each do |it|
     next if not it.identifier.to_s.end_with?(".html", ".md")
-    next if it[:is_hidden] # 404
-    next if part == 0 and it.identifier.to_s == "/index.html" # homepage
+    next if it[:is_hidden] # e.g. 404
+    next if it[:no_menu] # e.g. contact
+    next if it.identifier.to_s == "/index.html" # homepage
 
+    # defaults
     subsection = it.identifier.components[part].split(".")[0]
+    menu_name = it[:menu_name] ? it[:menu_name] : subsection.capitalize
+    link = section + subsection + "/"
+    menu_weight = it[:menu_weight] ? it[:menu_weight] : 0
 
-    h[subsection] = { :text => it[:menu_name] ? it[:menu_name] : subsection.capitalize,
-                  :link => section + subsection + "/",
-                  :weight => it[:menu_weight] ? it[:menu_weight] : 0 }
+    # special case subsection index
+    if subsection == "index"
+      menu_name = "Overview"
+      link = section
+      menu_weight = 100
+    end
+
+    h[subsection] = { :text => menu_name, :link => link, :weight => menu_weight }
   end
 
   # sort by weight, default = 0 puts at end
