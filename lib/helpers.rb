73e950c3b0881c3494defae479a9c69329de8912
Carsten Teibes <dev@f4ke.de>
2022-06-25 23:00:53 +0200

    Download page: jenkins snapshots from master branch Add disclaimer to debug build Add additional headers to "Other" tab

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -14,3 +14,28 @@ def media_vid(file, title = nil)
 	end
 	return "[![[video]](/videos/thumbs/" + thumb + ")](/videos/" + file + " '" + title + "')"
 end
+
+# deprecated
+
+def link_to_jenkins_master(job)
+  api_url = "https://ci.easyrpg.org/view/Player/job/#{job}/lastSuccessfulBuild/api/json?tree=actions[buildsByBranchName[buildNumber]]"
+  build = "none"
+
+  res = Net::HTTP.get(URI(api_url))
+  if res
+    j = JSON.parse(res)
+    j["actions"].each{|action|
+      if action.key? "buildsByBranchName"
+        if action["buildsByBranchName"].key? "origin/master"
+          build = action["buildsByBranchName"]["origin/master"]["buildNumber"]
+        end
+      end
+    }
+  end
+  if ! build.is_a? Numeric
+    # fallback
+    build = "lastSuccessfulBuild"
+  end
+
+  return "https://ci.easyrpg.org/job/#{job}/#{build}/artifact"
+end
